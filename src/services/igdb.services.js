5e9fff0ed3a06d72fac9bf34dabd43efa03d61fc
Ali Hamza <hamza120025@gmail.com>
2024-04-09 07:31:25 +0100

    Add igdb.services.js file for fetching games from IGDB API

---
+++ src/services/igdb.services.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+export const getGames = async (next) => {
+  if (next) {
+    const response = await axios.get(next);
+    console.log(response.data);
+    return response.data;
+  }
+  const response = await axios.get(`${backendUrl}/games`);
+  return response.data.data;
+};
