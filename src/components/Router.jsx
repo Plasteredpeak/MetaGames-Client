91262fbea7529f31b909c1661171671e3abe6d8a
Ali Hamza <hamza120025@gmail.com>
2024-04-09 12:41:23 +0100

    Add authentication routes and pages

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,8 +2,16 @@ import React from "react";
 import { useRoutes, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import GamePage from "../pages/GamePage";
+import SignUp from "../pages/Auth/SignUp";
+import Login from "../pages/Auth/Login";
+
+const authRoutes = [
+  { path: "/signup", element: <SignUp /> },
+  { path: "/login", element: <Login /> },
+];
 
 const routes = [
+  ...authRoutes,
   { path: "*", element: <Navigate to="/home" replace /> },
   { path: "/home", element: <Home /> },
   { path: "/game/:id", element: <GamePage /> },
