fddada5f3f38f0e8e3dec1d3f8faa00fd9b57cb8
Ali Hamza <hamza120025@gmail.com>
2024-04-13 18:10:30 +0100

    Refactor axios requests in igdb.services.js for error handling and toast notifications

--- src/services/igdb.services.js
+++ src/services/igdb.services.js
@@ -1,42 +1,75 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const getGames = async (next) => {
-  if (next) {
-    const response = await axios.get(next);
-    console.log(response.data);
-    return response.data;
+  try {
+    if (next) {
+      const response = await axios.get(next);
+      console.log(response.data);
+      return response.data;
+    }
+    const response = await axios.get(`${backendUrl}/games`);
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Failed to fetch games. please reload");
   }
-  const response = await axios.get(`${backendUrl}/games`);
-  return response.data.data;
 };
 
 export const getGamesByGenre = async (genre) => {
-  const response = await axios.get(`${backendUrl}/games/?genres=${genre}`);
-  return response.data.data;
+  try {
+    const response = await axios.get(`${backendUrl}/games/?genres=${genre}`);
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Failed to fetch games by genre. please reload");
+  }
 };
 
 export const searchGamesByGenre = async (genre, queryString) => {
-  console.log("searching for games", queryString);
-  const response = await axios.get(
-    `${backendUrl}/games?genres=${genre}&search=${queryString}`,
-  );
-  return response.data.data;
+  try {
+    console.log("searching for games", queryString);
+    const response = await axios.get(
+      `${backendUrl}/games?genres=${genre}&search=${queryString}`,
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Failed to search games by genre. please reload");
+  }
 };
 
 export const getGenres = async () => {
-  const response = await axios.get(`${backendUrl}/games/genres`);
-  return response.data.data;
+  try {
+    const response = await axios.get(`${backendUrl}/games/genres`);
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Failed to fetch genres. please reload");
+  }
 };
 
 export const searchGames = async (queryString) => {
-  console.log("searching for games", queryString);
-  const response = await axios.get(`${backendUrl}/games?search=${queryString}`);
-  return response.data.data;
+  try {
+    console.log("searching for games", queryString);
+    const response = await axios.get(
+      `${backendUrl}/games?search=${queryString}`,
+    );
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Failed to search games. please reload");
+  }
 };
 
 export const getGameById = async (id) => {
-  const response = await axios.get(`${backendUrl}/games/${id}`);
-  return response.data.data;
+  try {
+    const response = await axios.get(`${backendUrl}/games/${id}`);
+    return response.data.data;
+  } catch (error) {
+    console.error(error);
+    toast.error("Failed to fetch game by ID. please reload");
+  }
 };
